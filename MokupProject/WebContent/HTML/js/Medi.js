397138e04655573affc0b7dc39c21eb218ddb936
Stanislav Iakimovskii <stanislav.iakimovskii@webgate.biz>
2015-09-16 15:46:39 +0200

    Angularjs Änderungen Suchfeld

--- MokupProject/WebContent/HTML/js/Medi.js
+++ MokupProject/WebContent/HTML/js/Medi.js
@@ -1,42 +1,41 @@
+angular.module('coag', [])
 
-angular.module('coag',[])
+.controller('mediCtrl', function($scope) {
+	$scope.allMedis = [];
+	$scope.mediName = "";
+	$scope.mediMg = "";
+
+	$scope.msgLocked = null;
+
+	$scope.helpme = "";
+	$scope.helpBtn = "Help";
+	$scope.addMedi = function() {
+		if ($scope.mediName !== "" && $scope.mediMg > 0) {
+			var medi = {};
+			medi.name = $scope.mediName;
+			medi.mg = $scope.mediMg;
+			$scope.allMedis.push(medi);
+			$scope.mediName = "";
+			$scope.mediMg = "";
+		}
+	};
     
-.controller('mediCtrl', function($scope){
-   $scope.allMedis = [];
-    $scope.mediName = "";
-    $scope.mediMg = "";
-    
-      $scope.msgLocked =null;
-    
-  $scope.helpme = "";
-    $scope.helpBtn = "Help";
-    $scope.addMedi = function(){
-        if($scope.mediName !== "" && $scope.mediMg > 0){
-            var medi = {};
-                medi.name = $scope.mediName;
-                medi.mg = $scope.mediMg;
-                $scope.allMedis.push(medi);
-                $scope.mediName = "";
-                $scope.mediMg = "";
-        }
+    $scope.removeMedi = function(medi){
+    var i = $scope.allMedis.indexOf(medi);
+        $scope.allMedis.splice(i, 2);
     };
 
-  
-    
-    
-    
-    $scope.checkTimeInput = function(){
-          return $scope.msgLocked !== 'mediTime';  
-        };
-    
-        $scope.checkDateInput = function(){
-          return $scope.msgLocked !== 'mediDate';         };
-    
-    $scope.needHelp = function(){
-    
-    }
-    
+	$scope.checkTimeInput = function() {
+		return $scope.msgLocked !== 'mediTime';
+	};
 
-    
-    
-});;
+	$scope.checkDateInput = function() {
+		return $scope.msgLocked !== 'mediDate';
+	};
+
+	$scope.needHelp = function() {
+
+	}
+
+});
+;
--- MokupProject/WebContent/HTML/js/Reg.js
+++ MokupProject/WebContent/HTML/js/Reg.js
@@ -1,29 +1,33 @@
-angular.module('coag', [ 'ngResource' ]).factory('Reg',
-		[ '$resource', function($resource) {
-			return $resource('/inr/:id', null, {});
-		} ]).controller('regCtrl', [ '$scope', 'Reg', function($scope, Reg) {
-	$scope.name = "";
-	$scope.surname = "";
-	$scope.age = "";
-	$scope.sex = true;
-	$scope.norm = {
-		min : 0,
-		max : 6
-	};
+angular.module('coag',['ngResource'])
+    .factory('Reg', ['$resource', function($resource) {
+        return $resource('/inr/:id', null,{});
+    }])
+    .controller('regCtrl', ['$scope', 'Reg', function($scope, Reg){
+        $scope.name = "";
+        $scope.surname = "";
+        $scope.age = "";
+        $scope.sex = true;
+        $scope.norm = {
+            min : 0,
+            max : 6
+        };
+        
+       $scope.msgGreeting = null;
+          
+        $scope.regist = function(){
+                       
+            Reg.save({
+                Name: $scope.regNameme,
+                Surname: $scope.regSurname,
+                id: 13
+            })
+        };
+        $scope.checkNicknameInput = function(){
+          return $scope.msgGreeting !== 'nickname';  
+        };
+        
+  
+       
+    }]);;
+   
 
-	$scope.msgGreeting = null;
-
-	$scope.regist = function() {
-
-		Reg.save({
-			Name : $scope.regNameme,
-			Surname : $scope.regSurname,
-			id : 13
-		})
-	};
-	$scope.checkNicknameInput = function() {
-		return $scope.msgGreeting !== 'nickname';
-	};
-
-} ]);
-;
