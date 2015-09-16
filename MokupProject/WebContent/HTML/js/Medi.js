1cd601be73b2f106dd015ccd2af9918362fc6132
Stanislav Iakimovskii <stanislav.iakimovskii@webgate.biz>
2015-09-16 10:10:04 +0200

    Angular funktionen, alles funktioniert.

--- MokupProject/WebContent/HTML/js/Medi.js
+++ MokupProject/WebContent/HTML/js/Medi.js
@@ -1,20 +1,30 @@
+angular.module('coag', [])
 
-angular.module('coag',[])
-    
-.controller('mediCtrl', function($scope){
-   $scope.allMedis = [];
-    $scope.mediName = "";
-    $scope.mediMg = "";
-    
-    
-    $scope.addMedi = function(){
-        if($scope.mediName !== "" && $scope.mediMg > 0){
-            var medi = {};
-                medi.name = $scope.mediName;
-                medi.mg = $scope.mediMg;
-                $scope.allMedis.push(medi);
-                $scope.mediName = "";
-                $scope.mediMg = "";
-        }
-    }
+.controller('mediCtrl', function($scope) {
+	$scope.allMedis = [];
+	$scope.mediName = "";
+	$scope.mediMg = "";
+
+	$scope.msgLocked = null;
+
+	$scope.addMedi = function() {
+		if ($scope.mediName !== "" && $scope.mediMg > 0) {
+			var medi = {};
+			medi.name = $scope.mediName;
+			medi.mg = $scope.mediMg;
+			$scope.allMedis.push(medi);
+			$scope.mediName = "";
+			$scope.mediMg = "";
+		}
+	};
+
+	$scope.checkTimeInput = function() {
+		return $scope.msgLocked !== 'mediTime';
+	};
+
+	$scope.checkDateInput = function() {
+		return $scope.msgLocked !== 'mediDate';
+	};
+
 });
+;
--- MokupProject/WebContent/HTML/js/Reg.js
+++ MokupProject/WebContent/HTML/js/Reg.js
@@ -1,33 +1,29 @@
-angular.module('coag',['ngResource'])
-    .factory('Reg', ['$resource', function($resource) {
-        return $resource('/inr/:id', null,{});
-    }])
-    .controller('regCtrl', ['$scope', 'Reg', function($scope, Reg){
-        $scope.name = "";
-        $scope.surname = "";
-        $scope.age = "";
-        $scope.sex = true;
-        $scope.norm = {
-            min : 0,
-            max : 6
-        };
-        
-       $scope.msgGreeting = null;
-          
-        $scope.regist = function(){
-                       
-            Reg.save({
-                Name: $scope.regNameme,
-                Surname: $scope.regSurname,
-                id: 13
-            })
-        };
-        $scope.checkNicknameInput = function(){
-          return $scope.msgGreeting !== 'nickname';  
-        };
-        
-  
-       
-    }]);;
-   
+angular.module('coag', [ 'ngResource' ]).factory('Reg',
+		[ '$resource', function($resource) {
+			return $resource('/inr/:id', null, {});
+		} ]).controller('regCtrl', [ '$scope', 'Reg', function($scope, Reg) {
+	$scope.name = "";
+	$scope.surname = "";
+	$scope.age = "";
+	$scope.sex = true;
+	$scope.norm = {
+		min : 0,
+		max : 6
+	};
 
+	$scope.msgGreeting = null;
+
+	$scope.regist = function() {
+
+		Reg.save({
+			Name : $scope.regNameme,
+			Surname : $scope.regSurname,
+			id : 13
+		})
+	};
+	$scope.checkNicknameInput = function() {
+		return $scope.msgGreeting !== 'nickname';
+	};
+
+} ]);
+;
