bc6cc618d981c1499b0334b766888494a36ad772
Stanislav Iakimovskii <stanislav.iakimovskii@webgate.biz>
2015-11-27 15:50:24 +0100

    Shop aussehen angepasst

--- CoagulationControlApp/CoagulationControlApp-shared/src/main/java/biz/webgate/darwino/coag/rest/controller/UserController.java
+++ CoagulationControlApp/CoagulationControlApp-shared/src/main/java/biz/webgate/darwino/coag/rest/controller/UserController.java
@@ -4,7 +4,6 @@ import biz.webgate.darwino.coag.app.AppManifest;
 import biz.webgate.darwino.coag.bo.UserEntry;
 import biz.webgate.darwino.coag.dao.UserStorageService;
 import biz.webgate.darwino.coag.rest.RestResult;
-
 import com.darwino.commons.json.JsonException;
 import com.darwino.commons.json.JsonObject;
 import com.darwino.commons.services.HttpServiceContext;
@@ -20,14 +19,11 @@ public class UserController extends EndpointController<UserEntry> {
 
 	@Override
 	public void getOne(HttpServiceContext context, String unid) {
-		UserEntry entry = null;
-
 		try {
-			
+			UserEntry entry = null;
 			RestResult result = new RestResult();
 			try {
-				entry = service.getObjectByUNID(unid,
-						AppManifest.getDatabase());
+				entry = service.getObjectByUNID(unid, AppManifest.getDatabase());
 				result.setUserData(entry);
 				result.setStatus("ok");
 			} catch (JsonDBException jex) {
@@ -38,22 +34,10 @@ public class UserController extends EndpointController<UserEntry> {
 		} catch (JsonException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
-	@Override
-	public void getMany(HttpServiceContext context) {
-		try{
-			RestResult result = new RestResult();
-			
-			result.setStatus("error");
-			result.setError("User does not support query requests");
-			
-			processToJson(context, result);
-		} catch( JsonException ex) {
-			ex.printStackTrace();
-		}
-	}
+
 
 	@Override
 	public void create(HttpServiceContext context) {
@@ -83,6 +67,25 @@ public class UserController extends EndpointController<UserEntry> {
 
 	@Override
 	public void update(HttpServiceContext context, String unid) {
+		UserEntry entry = new UserEntry();
+		JsonObject result = new JsonObject();
+		try {
+			entry = (UserEntry) processFromJson(context, entry);
+			
+			service.saveObject(entry, AppManifest.getDatabase());
+			
+			result.put("status", "ok");
+
+		} catch (JsonException e) {
+			e.printStackTrace();
+			result.put("status", "error");
+		}
+		context.emitJson(result);
+
+	}
+
+	@Override
+	public void getMany(HttpServiceContext context) {
 		// TODO Auto-generated method stub
 		
 	}
